9d2c8e4620833aff65237ea0aff348d4f603f738
Dragan Filipovic <info@frontenddot.com>
2015-01-09 22:01:18 +0100

    Basic styles

--- client/js/app.js
+++ client/js/app.js
@@ -3,22 +3,22 @@ app = {
 
 		app.showPosts();
 
-		$('section > button').on('click', function(event) {
+		$('.create').on('click', function(event) {
 			event.preventDefault();
 			var itemTitle = $('input').val();
 			var itemContent = $('textarea').val();
 	 		var jsonItem = JSON.stringify({title: itemTitle, content: itemContent});
 			app.createPost(jsonItem);
 		});
-		$('ul').on('click', 'a', function(event) {
+		$('section').on('click', '.delete', function(event) {
 			event.preventDefault();
 			app.deletePost($(this).parent().attr('id'));
 		});
-		$('ul').on('click', 'li', function(event) {
-			app.showPost($(this).attr('id'));
+		$('section').on('click', 'li > p', function(event) {
+			app.showPost($(this).parent().attr('id'));
 		});
-		$(document).on('click', 'article button', function(event) {
-			var newTitle = $(this).siblings('h2').text();
+		$('article').on('click', '.update', function(event) {
+			var newTitle = $(this).siblings('h3').text();
 			var newContent = $(this).siblings('div').text();
 	 		var jsonItem = JSON.stringify({title: newTitle, content: newContent});
 			app.updatePost($(this).parent().data('id'), jsonItem);
@@ -28,14 +28,22 @@ app = {
 		//READ: get all items from API uri
 		$.get('/api/items', function(data) {
 			$.each(data, function(index, val) {
-				$('<li id="'+val._id+'"><p>'+val.title+'</p> <a href="">delete</a></li>').appendTo('ul');
+				$('<li id="'+val._id+'"><p>'+val.title+'</p> <button class="delete">delete</button></li>').appendTo('ul');
 			});
+			if (data.length != 0) {
+				//Show one full item
+				$('article').data('id',data[0]._id).html('');
+				$('<h3 contenteditable="true">'+data[0].title+'</h3><div contenteditable="true">'+data[0].content+'</div><button class="update">update</button>').appendTo('article');
+			};
+			app.checkItems();
 		});
+		
 	},
 	showPost: function(itemId){
 		//GET: get single item from API uri
 		$.get('/api/items/'+itemId, function(data) {
-			$('<article data-id="'+data._id+'"  contenteditable="true"><h2>'+data.title+'</h2><div>'+data.content+'</div><p>Click on text and edit</p><button>save</button></article>').appendTo('section');
+			$('article').html('').data('id',data._id);
+			$('<h3 contenteditable="true">'+data.title+'</h3><div contenteditable="true">'+data.content+'</div><button class="update">update</button>').appendTo('article');
 		});	
 	},
 	createPost: function(data){
@@ -48,11 +56,11 @@ app = {
  			data: data
  		})
  		.done(function(data) {
- 			$('<li id="'+data._id+'"><p>'+data.title+'</p> <a href="">delete</a></li>').appendTo('ul');
+ 			$('<li id="'+data._id+'"><p>'+data.title+'</p> <button class="delete">delete</button></li>').appendTo('section > div > ul');
+ 			app.checkItems();
  		});
 	},
 	updatePost: function(itemId, newData){
-		console.log(itemId);
 		//UPDATE: update item
  		$.ajax({
  			url: '/api/items/'+itemId,
@@ -62,18 +70,32 @@ app = {
  			data: newData
  		})
  		.done(function(data) {
- 			console.log(data._id+' - '+data.title);
  			$('#'+data._id+' p').text(data.title);
  		});
 	},
 	deletePost: function(itemId){
 		//DELETE: delete item
+		if (itemId) {
+			var itemElement = document.getElementById(itemId);
+	 		itemElement.parentNode.removeChild(itemElement);
+	 		var fullItem = $('article');
+	 		if (fullItem.data('id') === itemId) {
+	 			fullItem.html('<h4>Select item to update: Click on item Title</h4>');
+	 		};			
+		};
  		$.ajax({
  			url: '/api/items/'+itemId,
  			type: 'DELETE'
  		});
- 		var itemElement = document.getElementById(itemId);
-		itemElement.parentNode.removeChild(itemElement);
+ 		app.checkItems();
+
+	},
+	checkItems: function() {
+		if ($('li').length === 0) {
+			$('ul').addClass('empty-list');
+		} else {
+			$('ul').removeClass('empty-list');
+		}
 	}
 }
 jQuery(document).ready(function($) {
